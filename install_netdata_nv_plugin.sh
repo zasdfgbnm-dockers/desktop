b3100e9aad2eddff197043026847b1500fc3015b
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-11-15 15:51:42 -0500

    fix pynvml install

--- install_netdata_nv_plugin.sh
+++ install_netdata_nv_plugin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pip install nvidia-ml-py
+cd /usr/lib/netdata/python.d/python_modules
+wget https://raw.githubusercontent.com/coraxx/netdata_nv_plugin/master/python_modules/pynvml.py
 
 cd /usr/lib/netdata/python.d/
 wget https://raw.githubusercontent.com/coraxx/netdata_nv_plugin/master/nv.chart.py
